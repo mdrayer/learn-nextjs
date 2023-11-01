d5a40132156cb9c9a0706aa7123e99bb9d0dd676
Michael Drayer <mdrayer@forumone.com>
2023-11-01 14:27:24 -0400

    Chapter 5: Navigation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Set output to export if you want to utilize Static Exports
+  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  // output: 'export',
+};
 
 module.exports = nextConfig;
